7e9d13ce992dcdafcdb3ab1400646fc8cc5779f3
juanjo.figue97 <juanjo.figue97@gmail.com>
2022-08-09 17:02:10 -0500

    formas de representar un string y su uso

---
+++ Cadenas de texto/Strings.js
@@ -0,0 +1,32 @@
+// Sesiòn 4 - String ( cadena de caracteres)
+
+let str_dbl = 'hola soy un texto con comillas simples';
+let str_sng = "hola soy un texto con comillas dobles";
+
+console.log(str_dbl);
+console.log(str_sng);
+
+let srt_comillas = "El otro dia me dijo literalmente \"ve a sacar la basura\" ";
+let srt_comillas_simples = 'El otro dia me dijo literalmente "ve a sacar la basura" ';
+
+
+/////// backticks
+
+let Nombre= 'Juan Jose';
+let saludo= `hola buenos dias ${Nombre} Bienvenido`
+
+console.log(saludo);
+
+// Plantillas HTML
+let plantilla = `
+<html>
+    <h1> Pagina web de ${Nombre} </h1>
+    <p> esto es un parrafo </p>
+</html>`;
+
+console.log(plantilla);
+
+///// Introduccion de variables en html
+
+let libros = ["Empieza por el por que", "El monje que vendio su Ferrari"," El poder del ahora"];
+
