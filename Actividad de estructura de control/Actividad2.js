179f8aa8632fa16b5125907cc23767cd8cd93183
juanjo.figue97 <juanjo.figue97@gmail.com>
2022-08-09 16:40:00 -0500

    Ejercicio de creacion de diccionarios

---
+++ Actividad de estructura de control/Actividad2.js
@@ -0,0 +1,29 @@
+let name = "Juan Jose Figueroa";
+const age = 25;
+IsProgrammer = true;
+const born = new Date(1997,2,4);
+const date1 = new Date(1999);
+
+book = {
+    title: "Prisoner of Azkaban",
+    author: "J. K. Rowling",
+    date: date1,
+    url : "https://es.wikipedia.org/wiki/Harry_Potter" 
+} 
+
+//born.setDate()
+console.log(name,"\n",age,"\n",IsProgrammer,"\n",born);
+console.log(book)
+
+const lista = [
+    "Gorka",
+    34,
+    true,
+    new Date(1987, 10, 20),
+    { 
+        titulo: "El monje que vendió su Ferrari",
+        autor: "Robin S. Sharma",
+        fecha: new Date(1996, 0, 1),
+        url: "https://www.amazon.es/dp/B0062XBS32/"
+      },
+  ];
