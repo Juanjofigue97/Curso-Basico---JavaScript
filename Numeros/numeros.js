f40b51fc3615d80ff508710488c4946b4e513102
juanjo.figue97 <juanjo.figue97@gmail.com>
2022-08-20 11:57:11 -0500

    Variables y precision

---
+++ Numeros/numeros.js
@@ -0,0 +1,18 @@
+// Variables numericas 
+
+let a = 5;
+
+console.log(a);
+
+let b = 0.1;
+
+console.log(b);
+
+// Precisiòn
+
+let c = 0.2;
+
+console.log(b+c);
+
+console.log(Math.round((b+c)*100)/100);
+
