e787e7a77f36071c9a90074fe51fb878fb5528d4
juanjo.figue97 <juanjo.figue97@gmail.com>
2022-08-23 10:02:03 -0500

    UNa funcion dentro de otra

---
+++ funciones/Funciones_sobrecarga.js
@@ -0,0 +1,20 @@
+// Carga y sobrecarga de funciones
+
+function saludar(){
+    hola()
+}
+function hola(){
+    console.log("hola soy la funcion hola()")
+}
+
+saludar()
+
+// global()
+// saludar() global()
+// hola() saludar () global()
+// saludar () global()
+// global()
+
+// function recursivo(){
+//     recursivo()
+// }
